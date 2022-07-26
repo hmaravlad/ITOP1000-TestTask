e38e182ba07553388693741e47a411a630cb2607
hmaravlad <hmara.vladislav2@gmail.com>
2022-07-26 21:34:58 +0300

    add error-box component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import AppComponent from './app.component';
 import CurrenciesModule from './currencies/currencies.module';
 import HeaderComponent from './header/header.component';
+import ErrorBoxComponent from './error-box/error-box.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    ErrorBoxComponent,
   ],
   imports: [
     BrowserModule,
