b8e121015ba790f4ab29c76918d1b640a104ac8e
hmaravlad <hmara.vladislav2@gmail.com>
2022-07-26 19:36:16 +0300

    add basic header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import AppComponent from './app.component';
 import CurrenciesModule from './currencies/currencies.module';
+import HeaderComponent from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
